092dee08864c584943d4c305fd0578181280796c
student <student@240-08.INFORMATYKA.local>
2016-10-13 14:42:41 +0200

    Zadanie 4, zajęcia nr 2, 13.10.2016

--- src/org/yourorghere/Marekszmolda.java
+++ src/org/yourorghere/Marekszmolda.java
@@ -105,16 +105,17 @@ System.exit(0);
  float x, y, kat;
   
 gl.glBegin(GL.GL_TRIANGLE_FAN);
-gl.glColor3f(0.5f,0.0f,1.0f);
+gl.glColor3f(0.2f,0.9f,1.0f);
 gl.glVertex3f(0.0f, 0.0f, -2.0f); //?rodek
 for (kat = 0.0f; kat < (2.0f * Math.PI);
 kat += (Math.PI / 32.0f)) {
 x = 1.5f * (float) Math.sin(kat);
 y = 1.5f * (float) Math.cos(kat);
-gl.glVertex3f(x, y, -2.0f); //kolejne punkty
+ gl.glVertex3f(x, y, 1.0f); //kolejne punkty
 }
 gl.glEnd();
 gl.glBegin(GL.GL_TRIANGLE_FAN);
+gl.glColor3f(0.9f,1.0f,1.0f);
 gl.glVertex3f(0.0f, 0.0f, 1.0f); //?rodek
 for (kat = (float)(2.0f * Math.PI) ; kat >0.0f ;
 kat -= (Math.PI / 32.0f)) {
@@ -123,16 +124,7 @@ y = 1.5f * (float) Math.cos(kat);
 gl.glVertex3f(x, y, 1.0f); //kolejne punkty
 }
 gl.glEnd();
-gl.glBegin(GL.GL_QUAD_STRIP);
-gl.glColor3f(0.1f,0.0f,1.0f);
-for (kat = 0.0f; kat < (2.0f * Math.PI);
-kat += (Math.PI / 32.0f)) {
-x = 1.5f * (float) Math.sin(kat);
-y = 1.5f * (float) Math.cos(kat);
- gl.glVertex3f(x, y, -2.0f); //kolejne punkty
- gl.glVertex3f(x, y, 1.0f);
-}
-gl.glEnd();
+
 
  // Flush all drawing operations to the graphics card
  gl.glFlush();
